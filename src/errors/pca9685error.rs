315e6a8feabb0f82a31d52e5c799f13028a4e937
Otto Korhonen <otto@xd.fi>
2026-02-03 20:38:47 +0200

    added content to pca9685error

--- src/errors/pca9685error.rs
+++ src/errors/pca9685error.rs
@@ -1,6 +1,29 @@
+use core::{fmt, error::Error};
 
-pub enum Pca9685Error<E>{
+#[derive(Debug)]
+pub enum Pca9685Error<E> {
     DeviceNotFound,
     I2CError(E),
-    DelayTimeOutOfScope
+    DelayTimeOutOfScope,
+}
+
+impl<E> fmt::Display for Pca9685Error<E>
+where
+    E: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::I2CError(e) => write!(f,"I2C error {:?}", e),
+            Self::DeviceNotFound => write!(f, "Device not found!"),
+            Self::DelayTimeOutOfScope => write!(f, "Delay time is out of scope")
+        }
+    }
+}
+
+impl <E> Error for Pca9685Error<E> where E: fmt::Debug{}
+
+impl<E> From<E> for Pca9685Error<E> {
+    fn from(e: E) -> Self {
+        Self::I2CError(e)
+    }
 }
